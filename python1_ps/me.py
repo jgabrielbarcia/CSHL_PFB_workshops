552b0829cf4b0a911d547f0a6749e1ec9fb2bee4
pfb26 <pfb26@info26.cshl.edu>
2023-10-17 11:41:44 -0400

    python 1 pset done

--- python1_ps/me.py
+++ python1_ps/me.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
-my_name = 'Gabriel'
+import sys
 
-print('My name:', my_name)
+my_name = sys.argv[1]
+my_color = sys.argv[2]
+my_activity = sys.argv[3]
+my_animal = sys.argv[4]
+
+print('My name is',my_name, ', my favorite color is',my_color, ', my favorite activity is',my_activity, ', and my favorite animal is',my_animal)
+
+my_name = 'Gabriel'
+my_color = 'Green'
+my_activity = 'Coding'
+my_animal = 'Dog'
+my_name2 = 'Jose Gabriel'
+
+print(f'My name: {my_name2}\nMy favorite color is: {my_color}')
+print('My favorite color:', my_color)
+print('My favorite activity:', my_activity)
+print('My favorite animal:', my_animal)
