81f51c9d615165e61b9dd3fc39ab7fcb09f705ad
Gabriel Barcia <jgabrielbarcia@Gabriels-MacBook-Pro.local>
2023-10-18 21:33:18 -0400

    all pset 4

---
+++ python4_ps/list_comprehension.py
@@ -0,0 +1,18 @@
+#! usr/bin/env python3
+
+import sys
+import math
+
+#i = 0
+#nums = []
+#for i in range(0,100):
+ #i = i + 1
+ #nums.append(i)
+
+#print(nums)
+
+count_to_100 = [i for i in range(101)[1:101]]
+
+print(count_to_100)
+
+
---
+++ python4_ps/while_loop_1_100.py
@@ -0,0 +1,10 @@
+#! usr/bin/env python3
+
+import sys
+import math
+
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
